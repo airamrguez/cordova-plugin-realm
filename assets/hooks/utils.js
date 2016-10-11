43228ce262c8de55f6d644ade11b5699967f4a52
Airam <airamrguezrivero@gmail.com>
2016-10-11 22:06:23 +0200

    hook to create native objects model

---
+++ assets/hooks/utils.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var ejs = require('./ejs');
+
+var getRawTemplate = function(platform) {
+    var filename;
+    switch (platform) {
+        case 'ios':
+            filename = 'Objc.tpl';
+            break;
+        case 'java':
+            filename = 'Java.tpl';
+            break;
+        default:
+            throw new Error('platform ' + platform + ' not supported.');
+    }
+    return fs.readFileSync('hooks/realm/templates/' + filename + '.ejs', 'utf8');
+};
+
+module.exports = {
+    getTemplate: function(platform) {
+        return ejs.compile(getRawTemplate(platform));
+    },
+    getFileExtension: function(platform) {
+        switch (platform) {
+            case 'ios':
+                return 'h';
+            case 'android':
+                return 'java';
+            default:
+                throw new Error('platform ' + platform + ' not supported.');
+        }
+    },
+    writeFile: function(path, content, cb) {
+        fs.writeFile(path, content, function(error) {
+            if (error) {
+                cb(error);
+                return;
+            }
+            cb(null);
+        });
+    }
+};
