d0220847bc978fde674ce85348ea8d0975473c6d
Airam <airamrguezrivero@gmail.com>
2016-10-12 17:52:29 +0200

    map js types to obj-c types

--- assets/hooks/createRealmObjects.js
+++ assets/hooks/createRealmObjects.js
@@ -15,9 +15,80 @@ module.exports = function(context) {
     'src'
   );
 
-  // TODO
-  function objectiveCType(type) {
-    return 'NSString*';
+  function typeExists(type, schemas) {
+    return schemas.some(function(schema) {
+      return schema.name === type;
+    });
+  }
+
+  function objectiveCTypeForNonOptionalType(propType, schemas) {
+    var type = propType.type;
+    switch (type) {
+      case 'bool':
+        return 'BOOL ';
+      case 'int':
+        return 'NSInteger ';
+      case 'float':
+        return 'float ';
+      case 'double':
+        return 'double ';
+      case 'string':
+        return 'NSString *';
+      case 'date':
+        return 'NSDate *';
+      case 'list': {
+        var objectType = propType.objectType;
+        if (typeExists(objectType, schemas)) {
+          return 'RLMArray<' + objectType + ' *><' + objectType + '> *';
+        }
+        throw new Error('type ' + propType.objectType + ' not found in schema');
+      }
+      default: {
+        throw new Error('unsupported type ' + type);
+      }
+    }
+  }
+
+  function objectiveCTypeForOptionalType(type, schemas) {
+    switch (type) {
+      case 'bool':
+        return 'NSNumber<RLMBool> *';
+      case 'int':
+        return 'NSNumber<RLMInt> *';
+      case 'float':
+        return 'NSNumber<RLMFloat> *';
+      case 'double':
+        return 'NSNumber<RLMDouble> *';
+      case 'string':
+        return 'NSString *';
+      case 'date':
+        return 'NSDate *';
+      default:
+        return type + ' *';
+    }
+  }
+
+  /**
+   * objectiveCType given a js type this function returns a equivalent objective-c type
+   * @param {string} type is a javascript type.
+   * @return {string} a equivalent objective-c type
+   */
+  function objectiveCType(propType, schemas) {
+    if (typeof propType === 'string') {
+      return objectiveCTypeForNonOptionalType({type: propType}, schemas);
+    } else if (typeof propType === 'object') {
+      if (propType.optional && propType.type === 'list') {
+        throw new Error('A list type cannot be optional. Check propType ' +
+          propType
+        );
+      }
+      // object properties are always optional and do not need an optional designation.
+      if (propType.optional || typeExists(propType.type, schemas)) {
+        return objectiveCTypeForOptionalType(propType.type, schemas);
+      }
+      return objectiveCTypeForNonOptionalType(propType, schemas);
+    }
+    throw new Error('invalid property ' + propType);
   }
 
   // TODO
@@ -28,9 +99,9 @@ module.exports = function(context) {
   function renderPlatformFile(platform, schemas) {
     var template = utils.getTemplate(platform);
     var objectsModel = template({
-      schemas,
-      objectiveCType,
-      javaType
+      schemas: schemas,
+      objectiveCType: objectiveCType,
+      javaType: javaType
     });
     var ext = utils.getFileExtension(platform);
     var fileName = 'Models.' + ext;
